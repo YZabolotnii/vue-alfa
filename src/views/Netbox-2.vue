abd4002a47dae5988a3d545530d6fe29fb3db23c
Yaroslav <131529206+YZabolotnii@users.noreply.github.com>
2024-11-11 12:22:22 +0200

    Create new page

---
+++ src/views/Netbox-2.vue
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+<template>
+  <div class="max-w-7xl px-10 max-[1028px]:px-6 mx-auto">
+    <div class="w-full flex justify-between max-w-4xl mx-auto my-8">
+      <div class="w-1/2">
+        <div class="text-white text-[60px] font-bold text-center mb-10">NETBOX 2 LITE</div>
+        <div
+          class="rounded-3xl p-10 text-white text-5xl text-center"
+          style="background: linear-gradient(90deg, rgba(3, 40, 238, 0.30) 0%, rgba(1, 23, 136, 0.30) 100%)"
+        >120 000 гривень</div>
+      </div>
+      <div
+        style="background: rgba(255, 255, 255, 0.60); box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+        class="rounded-3xl p-16  text-center"
+      >
+        <img src="/g-2.png" alt="">
+      </div>
+    </div>
+    <div>
+      <div class="text-white ">
+        NetBox2 Lite — це нове покоління компактних інтелектуальних приймачів GNSS, розроблених для різноманітних геодезичних проектів із використанням найновіших функцій GNSS. Цей приймач оснащений сучасними необхідними технологіями, такими як модуль Bluetooth, датчик нахилу IM, внутрішній літійіонний акумулятор ємністю 6800 мАг тощо. може працювати в різних режимах роботи (Мережевий RTK, Статичний і PPK) на основі різних необхідних точності та умови.
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
